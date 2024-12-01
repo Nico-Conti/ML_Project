0305ebd1c34cb72e970435cf0518216d9ea87f29
nico-conti <nico@LAPTOP-Nico.>
2024-12-01 11:28:31 +0100

    Update feature: Layer structure update

--- src/layer.py
+++ src/layer.py
@@ -16,16 +16,22 @@ class LayerDense():
 
         return self.output
 
-    def backward(self, upstream_delta):
+    def backward(self, upstream_delta, learning_rate):
+        
         # Apply the derivative of the activation function
         derivative_output = self.activation.derivative_fun(self.net)
-        
-        # Calculate the delta for this layer
-        delta = upstream_delta * derivative_output
+        if derivative_output.shape[1] == 1: derivative_output = np.reshape(derivative_output, derivative_output.shape[0])
 
-        self.grad_biases = delta
-        self.grad_weights = np.outer(self.inputs, delta)
-        
-        new_upstream_delta = delta.dot(self.weights.T)
-        
+        # Calculate the delta for this layer
+        delta = upstream_delta * derivative_output 
+
+        self.grad_biases = np.sum(delta, axis=0)
+        self.grad_weights = np.dot(self.inputs.T,delta) 
+        if self.weights.shape[1] == 1: self.grad_weights = self.grad_weights.reshape(self.weights.shape)
+
+        if len(delta.shape) == 1: delta = delta.reshape(delta.shape[0], 1)
+        new_upstream_delta = np.dot(delta, self.weights.T)
+        self.bias -= self.grad_biases * learning_rate
+        self.weights -= self.grad_weights * learning_rate
+
         return new_upstream_delta
