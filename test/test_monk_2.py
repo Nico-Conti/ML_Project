823d862c95addff228fccbe600b4d4b50dd0a4f0
nico-conti <nico@LAPTOP-Nico.>
2024-12-13 17:02:08 +0100

    monk test files

--- test/test_monk_2.py
+++ test/test_monk_2.py
@@ -14,12 +14,12 @@ import numpy as np
 script_dir = os.path.dirname(__file__)
 
 # Construct the relative path to the data file
-monk_1_train = os.path.join(script_dir, "../data/monk+s+problems/monks-1.train")
-monk_1_test = os.path.join(script_dir, "../data/monk+s+problems/monks-1.test")
+monk_2_train = os.path.join(script_dir, "../data/monk+s+problems/monks-2.train")
+monk_2_test = os.path.join(script_dir, "../data/monk+s+problems/monks-2.test")
 
 # Read the data using the constructed path
-x, y =  read_monk_data(monk_1_train)
-x_test, y_true = read_monk_data(monk_1_test)
+x, y =  read_monk_data(monk_2_train)
+x_test, y_true = read_monk_data(monk_2_test)
 x = feature_one_hot_encoding(x, [3,3,2,3,4,2])
 x_test = feature_one_hot_encoding(x_test, [3,3,2,3,4,2])
 
--- test/test_monk_3.py
+++ test/test_monk_3.py
@@ -14,12 +14,12 @@ import numpy as np
 script_dir = os.path.dirname(__file__)
 
 # Construct the relative path to the data file
-monk_1_train = os.path.join(script_dir, "../data/monk+s+problems/monks-1.train")
-monk_1_test = os.path.join(script_dir, "../data/monk+s+problems/monks-1.test")
+monk_3_train = os.path.join(script_dir, "../data/monk+s+problems/monks-3.train")
+monk_3_test = os.path.join(script_dir, "../data/monk+s+problems/monks-3.test")
 
 # Read the data using the constructed path
-x, y =  read_monk_data(monk_1_train)
-x_test, y_true = read_monk_data(monk_1_test)
+x, y =  read_monk_data(monk_3_train)
+x_test, y_true = read_monk_data(monk_3_test)
 x = feature_one_hot_encoding(x, [3,3,2,3,4,2])
 x_test = feature_one_hot_encoding(x_test, [3,3,2,3,4,2])
 
